a9674d98e8f3a1cfb360b28d13068368248ceb47
MenaSJ <Mena_JS9@outlook.com>
2025-02-07 12:31:50 -0600

    updated cart

--- guitarlaJS/src/components/Header.jsx
+++ guitarlaJS/src/components/Header.jsx
@@ -1,4 +1,4 @@
-export default function Header () {
+export default function Header ({cart}) {
     return (
         <header className="py-5 header">
         <div className="container-xl">
@@ -15,7 +15,9 @@ export default function Header () {
                         <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" className="bg-white p-3">
-                            <p className="text-center">El carrito esta vacio</p>
+                            {cart.length === 0 ? 
+                            (<p className="text-center">El carrito esta vacio</p>):
+                            (<>
                             <table className="w-100 table">
                                 <thead>
                                     <tr>
@@ -27,13 +29,14 @@ export default function Header () {
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    {cart.map(guitar => (
                                     <tr>
                                         <td>
-                                            <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
+                                            <img className="img-fluid" src={`./public/img/${guitar.image}.jpg`} alt="imagen guitarra" />
                                         </td>
-                                        <td>SRV</td>
+                                        <td>{guitar.name}</td>
                                         <td className="fw-bold">
-                                                $299
+                                                ${guitar.price}
                                         </td>
                                         <td className="flex align-items-start gap-4">
                                             <button
@@ -59,11 +62,14 @@ export default function Header () {
                                             </button>
                                         </td>
                                     </tr>
+                                    ))}
                                 </tbody>
                             </table>
 
                             <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
                             <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                            </>
+                        )}
                         </div>
                     </div>
                 </nav>
