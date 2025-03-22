15dbf48028270a057fe10e586efb96810e1fb20c
MenaSJ <Mena_JS9@outlook.com>
2025-03-22 01:26:43 -0600

    update

--- calculadora_propinas/src/App.tsx
+++ calculadora_propinas/src/App.tsx
@@ -26,20 +26,17 @@ function App() {
           </div>
         </div>
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          {order.length > 0 ?(
+          {order.length > 0 ? (
             <>
               <OrderContents order={order} removeItem={removeItem} />
 
-              <TipPercentageForm 
-                setTip={setTip}
-                tip={tip}
-              />
-                
-              <OrderTotals order={order} tip={tip} placeOrder={placeOrder}/>
-            </> ) : (
-              <p className="text-center">La orden esta vacia</p>
-            )
-          }
+              <TipPercentageForm setTip={setTip} tip={tip} />
+
+              <OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
+            </>
+          ) : (
+            <p className="text-center">La orden esta vacia</p>
+          )}
         </div>
       </main>
     </>
